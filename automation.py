d925af027602050d418a4d22758f3a1aa62f3150
Vision295 <theophilewachtel@gmail.com>
2025-12-01 13:17:09 +0900

    beginning of the automation

---
+++ automation.py
@@ -0,0 +1,69 @@
+import json
+from tkinter import CURRENT
+from gns3fy import Gns3Connector, Project, Node
+import time
+from generator import *
+
+SWITCH_TEMPLATE_NAME = "Ethernet switch"
+PC_TEMPLATE_NAME = "gossiptcpudp"
+# Connect to GNS3 server
+server = Gns3Connector(url="http://localhost:3080")
+
+
+
+
+
+mesh_info = {
+     TopologyType.FULL_MESH: {"S": (3, 15), "M": (10, 50), "L": (20, 100),"XL": (40, 200)},
+     TopologyType.BUS: {
+          "S": (7, 14), "M": (25, 50), "L": (50, 100), "XL": (100, 200),
+          "L1to1": (50, 50),"XL1to1": (100, 100)},
+     TopologyType.CLUSTERED2: {"S": (3, 15), "M": (10, 50), "L": (20, 100),"XL": (40, 200)}, 
+     TopologyType.CLUSTERED3: {"S": (3, 15), "M": (10, 50), "L": (20, 100),"XL": (40, 200)}, 
+#      TopologyType.RANDOM,
+#      TopologyType.HIERARCHICAL
+}
+protocol_list = ["UDP", "TCP"]
+links_list = [{} for i in range(10)]
+
+mesh, size = "", ""
+nb_switch, nb_pc = 0, 0
+protocol = ""
+links = {}
+
+intent_base = {
+      mesh+size:{
+            SWITCH_TEMPLATE_NAME: nb_switch,
+            PC_TEMPLATE_NAME: nb_pc,
+            "ip_range": "172.19.0.100/24",
+            "protocol": protocol,
+            "mesh": mesh,
+            "links": links
+      }
+}
+
+
+
+
+project = Project(
+    name="my_empty_project",
+    connector=server
+)
+project.create()        # type: ignore
+project.open()          # type: ignore
+project.get()           # type: ignore
+print("Project created:", project.project_id)
+
+with open("json/intent.json", "r") as file:
+    data = json.load(file)  # parses JSON into a Python dict or list
+
+topo = TopologyGenerator(TopologyType.FULL_MESH, data, "my_empty_project", "fullmesh")
+
+
+for node in project.nodes:
+      node.start()
+
+
+time.sleep(120)
+project.close()
+project.delete()
